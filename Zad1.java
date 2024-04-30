6ac12cc6ea9dbff6863f8bebbba7fe7c3f9af287
Michał Lenard <lenardmichal02@gmail.com>
2024-04-30 13:55:17 +0200

    zadania

--- Zad1.java
+++ Zad1.java
@@ -3,23 +3,26 @@ import java.util.Scanner;
 public class Zad1 {
     public static void main(String[] args) {
         Scanner wczytaj=new Scanner(System.in);
-        double o, średnia=0,p=0;
-        int i=-1;
+        double średnia=0;
+        int i = 0, o = -1, p;
+        int[] oceny = new int[30];
         String przedmiot;
         System.out.print("Podaj nazwę przedmiotu:");
         przedmiot=wczytaj.nextLine();
-
-        do{
+        do {
             System.out.print("Podaj ocene:");
-            o=wczytaj.nextDouble();
-            p=p+o;
+            oceny[i] = wczytaj.nextInt();
             i++;
-        } while(o>0);
-        średnia=p/i;
-        System.out.print("średnia z "+przedmiot+" wynosi:"+średnia);
-        
+            o++;
+        } while(i < oceny.length && oceny[i - 1] != 0);
         
         wczytaj.close();
-    }
 
+        for(p=0;p<o;p++){
+            średnia +=oceny[p];
+        }    
+        średnia=średnia/o;
+        System.out.print("średnia z "+przedmiot+" wynosi:"+średnia);
+    }
+    
 }
