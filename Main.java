306cd5c1e878351e21e242e4c59f7ace2c5c3b5a
Michał Lenard <lenardmichal02@gmail.com>
2024-04-30 20:35:21 +0200

    zad1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(4, 8);
+
+        System.out.println("Współrzędna x: " + point.getX());
+        System.out.println("Współrzędna y: " + point.getY());
+    }
+}
