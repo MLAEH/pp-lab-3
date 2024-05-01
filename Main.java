fd732d15a0b436aa241bcb54d474be7b51d89f73
Michał Lenard <lenardmichal02@gmail.com>
2024-05-01 12:13:48 +0200

    zad3

---
+++ Main.java
@@ -0,0 +1,23 @@
+import geometry.Circle;
+import geometry.ColoredCircle;
+import geometry.Point;
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(4, 8);
+
+        System.out.println("Współrzędna x: " + point.getX());
+        System.out.println("Współrzędna y: " + point.getY());
+        
+        Point center = new Point(1, 2);
+        Circle circle = new Circle(center, 4);
+
+        System.out.println("Środek: "+circle.getCenter().getX()+','+circle.getCenter().getY());
+        System.out.println("Promień: "+circle.getRadius());
+        System.out.println("Obwód koła: "+circle.calculatePerimeter());
+        System.out.println("Pole koła: "+circle.getArea());
+
+        ColoredCircle coloredCircle = new ColoredCircle(center, 4, "czerwony");
+        System.out.println("Kolor:"+coloredCircle.getColor());
+    }
+}
